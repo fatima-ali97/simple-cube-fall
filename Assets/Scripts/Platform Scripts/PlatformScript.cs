b159d76889348d17d013b8983ccb45f5883ac476
Fatima <202304056@student.polytechnic.bh>
2026-03-13 15:46:43 +0300

    add sound to settings view

--- Assets/Scripts/Platform Scripts/PlatformScript.cs
+++ Assets/Scripts/Platform Scripts/PlatformScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public float move_speed = 2f; 
     public float  bound_y = 6.0f;
 
